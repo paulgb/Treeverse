6c3699fe4f7f025712df0e4475f2e97e6ad0d789
Paul <paul@Pauls-MacBook-Air.local>
2016-10-27 21:35:11 -0400

    switch from make to gulp

---
+++ gulpfile.js
@@ -0,0 +1,52 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var concat = require("gulp-concat");
+var newer = require("gulp-newer");
+var download = require("gulp-download");
+var fs = require('fs');
+
+gulp.task("deps", function () {
+    if (!fs.existsSync("src/lib/d3.v4.min.js")) {
+        download("https://d3js.org/d3.v4.min.js")
+            .pipe(gulp.dest("src/lib/"));
+    }
+    if (!fs.existsSync("src/lib/d3-hierarchy.v1.min.js")) {
+        download("https://d3js.org/d3-hierarchy.v1.min.js")
+            .pipe(gulp.dest("src/lib/"));
+    }
+
+    gulp.src("src/lib/*.js")
+        .pipe(newer("extension/script/lib.js"))
+        .pipe(concat("lib.js"))
+        .pipe(gulp.dest("extension/script/"));
+});
+
+
+var backgroundProject = ts.createProject("src/background/tsconfig.json");
+
+gulp.task("background", function () {
+    return backgroundProject.src()
+        .pipe(newer('extension/script/background.js'))
+        .pipe(backgroundProject())
+        .js.pipe(concat('background.js'))
+        .pipe(gulp.dest("extension/script/"));
+});
+
+var viewerProject = ts.createProject("src/viewer/tsconfig.json");
+
+gulp.task("viewer", function () {
+    return viewerProject.src()
+        .pipe(newer('extension/script/viewer.js'))
+        .pipe(viewerProject())
+        .js.pipe(concat('viewer.js'))
+        .pipe(gulp.dest("extension/script/"));
+});
+
+gulp.task("scripts", ["viewer", "background"]);
+
+gulp.task("watch", ["scripts"], function () {
+    gulp.watch('src/viewer/*.ts', ["viewer"]);
+    gulp.watch('src/background/*.ts', ["background"]);
+});
+
+gulp.task("default", ["scripts"]);
