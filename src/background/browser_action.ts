626bbe17bab3d675834eb2dad83f30cc3cb99ea4
Paul Butler <paulgb@gmail.com>
2017-01-20 19:27:47 -0500

    switches to pageaction

--- src/background/browser_action.ts
+++ src/background/browser_action.ts
@@ -1,24 +1,40 @@
+namespace background {
+    let matchTweetURL = 'https?://twitter.com/(.+)/status/(\\d+)';
+    let matchTweetURLRegex = new RegExp(matchTweetURL);
 
-function getUserAndTweetFromUrl(url: string): [string, string] {
-    var parser = document.createElement('a');
-    parser.href = url;
-    if (parser.hostname != 'twitter.com') {
-        return null;
-    }
-    let match = /\/(.+)\/status\/(\d+)/.exec(parser.pathname);
-    if (match) {
-        return [match[1], match[2]];
-    }
-}
-
-chrome.browserAction.onClicked.addListener(
-    function (tab: chrome.tabs.Tab) {
-        let userTweetPair = getUserAndTweetFromUrl(tab.url);
-        if (!userTweetPair) {
-            return;
+    function getUserAndTweetFromUrl(url: string): [string, string] {
+        let match = matchTweetURLRegex.exec(url);
+        if (match) {
+            return [match[1], match[2]];
         }
-        let [username, tweetId] = userTweetPair;
+    }
 
-        let url = `resources/view.html#${username},${tweetId}`;
-        chrome.tabs.create({ 'url': chrome.extension.getURL(url) });
+    chrome.pageAction.onClicked.addListener(
+        function (tab: chrome.tabs.Tab) {
+            let userTweetPair = getUserAndTweetFromUrl(tab.url);
+            if (!userTweetPair) {
+                return;
+            }
+            let [username, tweetId] = userTweetPair;
+
+            let url = `resources/view.html#${username},${tweetId}`;
+            chrome.tabs.create({ 'url': chrome.extension.getURL(url) });
+        });
+
+    chrome.runtime.onInstalled.addListener((callback) => {
+        (<any>chrome).declarativeContent.onPageChanged.removeRules(undefined, () => {
+            (<any>chrome).declarativeContent.onPageChanged.addRules([
+                {
+                    conditions: [
+                        new (<any>chrome).declarativeContent.PageStateMatcher({
+                            pageUrl: {
+                                urlMatches: matchTweetURL
+                            }
+                        })
+                    ],
+                    actions: [new (<any>chrome).declarativeContent.ShowPageAction()]
+                }
+            ]);
+        });
     });
+}
