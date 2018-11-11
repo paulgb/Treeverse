c07e7d8aeb7bfee866590f8507a27904cbe7f752
Paul Butler <paulgb@gmail.com>
2018-11-10 22:16:16 -0500

    simlinks instead of copying

--- build_extensions.sh
+++ build_extensions.sh
@@ -2,13 +2,7 @@
 
 ./node_modules/.bin/webpack
 
-#cp -r extension_common/* extension_chrome/
-#cp -r extension_common/* extension_firefox/
+cd extension_chrome; zip -r ../extension_chrome.zip *; cd ..
 
-cd extension_chrome
-zip -r ../extension_chrome.zip *
-cd ..
+cd extension_firefox; zip -r ../extension_firefox.zip *; cd ..
 
-cd extension_firefox
-zip -r ../extension_firefox.zip *
-cd ..
